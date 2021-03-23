96246314b5310f1a33501da0ec261e92581ebf1d
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-23 18:16:55 -0400

    commit tree bb96dd1484cdf74f9db23a0306c2b4d27571915a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function ( event ) {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  } );
+
+  let paraText = document.querySelector(text);
+  paraText.innterHTML = 'JavaScript is so cool. It lets me add text to my page programmatically.';
+
+  paraText.innerHTML = 'This is really cool!';
